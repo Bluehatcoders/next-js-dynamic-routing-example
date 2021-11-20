f836946a70f298fdd0ab6b0c65e0e3f8e0a01b58
Satyanchal <62104921+Bluehatcoders@users.noreply.github.com>
2021-11-20 07:54:09 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,20 @@ import Header from '../components/header'
 const Home = () => (
   <>
     <Header />
-    <h1>Hello Soamee!</h1>
+   
+  
+    <div className="appHeader" style={{borderBottom: '0px solid #DCDCE9'}}>
+    <div className="left">
+      <div className="chip chip-danger ms-05 mb-05" style={{marginTop: '2px'}}>
+        <span className="chip-label">Happy</span>
+      </div>
+    </div>
+    <div className="pageTitle"><h1 alt="logo" className="logo" style={{fontSize: '28.5px', color: '#6236ffd4', maxHeight: '27.01px', cursor: 'pointer'}}>Alpha</h1></div>
+    <div className="right">
+    </div>
+  </div>
+  
+  
   </>
 )
 
